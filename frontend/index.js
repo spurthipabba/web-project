90858ae5adff724599569fcd500f4e46aa3f1a0e
Spurthi Pabba <spurthipabba@gmail.com>
2026-08-13 16:57:42 +0530

    index js commited

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,29 @@
-alert("hello from js");
+// alert("hello from js")
+const users = [
+ {
+   "name" : "Jonn Doe",
+   "gender" : "Male",
+   "image" : "john.png"
+ },
+   {
+   "name" : "Jane Doe",
+   "gender" : "Feale",
+   "image" : "jane.png"
+ }
+]
+
+
+var curIndex = 0;
+
+
+function toggle(){
+ if(curIndex == 0)
+   curIndex = 1;
+ else
+   curIndex = 0;
+
+
+ document.getElementById("card-image").src = users[curIndex].image
+ document.getElementById("card-name").innerText =users[curIndex].name
+ //gender needs to be updated
+}
