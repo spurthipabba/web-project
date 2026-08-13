66471cdce951855cb00a841ec460f00d60a34c7a
Spurthi Pabba <spurthipabba@gmail.com>
2026-08-13 15:33:13 +0530

    updated with frontend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express =require('express')
 const app=express()
-const port=8080
+const port= process.env.PORT ||8080
+app.use(express.static("frontend"))
 app.listen(port,function(){
   console.log("my app is running at http://localhost:"+port)
 })
